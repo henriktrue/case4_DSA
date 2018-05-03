557f0fbdb1566b99d29213b4cb5bdc8a46b11e1e
henriktrue <henriktrue@gmail.com>
2018-05-03 20:01:56 +0200

    pålagt filter
    winning

--- case4.m
+++ case4.m
@@ -22,6 +22,7 @@ Ts = time_S(2)-time_S(1);
 fs = 1/Ts; %800 hertz
 N = 8192;
 Tdur = N/fs;
+n = (1:N);
 
 %% plot af målinger uredigeret
 figure
@@ -41,4 +42,47 @@ xlim([-5 5]), ylim([0 0.5]);
 lgd = legend('IR respons');
 title(lgd,'akser');
 
-%% filtrering af signaler
+%% filtrering af Simons målinger
+% rød lampesignal
+M_S_R = 40;                           % filterkoefficienter
+hMA_R_S = 1/M_S_R*ones(1,M_S_R);          % MA-filter, filterkoefficienter
+yMA_R_S = filter(hMA_R_S,1,RED_S);      % filtrerer inputsignal for Simons del
+
+% IR lampesignal
+M_S_IR = 3;                           % filterkoefficienter
+hMA_IR_S = 1/M_S_IR*ones(1,M_S_IR);          % MA-filter, filterkoefficienter
+yMA_IR_S = filter(hMA_IR_S,1,IR_S);      % filtrerer inputsignal for Simons del
+
+c = zeros(1,N);
+
+for i = 1:512
+    c((i*16-15):(16*i)) = ones(1,16) * max(yMA_IR_S(16*i-5:16*i));
+end
+
+% IR lampesignal
+M_S_IR_filtreret = 280;                           % filterkoefficienter
+hMA_IR_S_filtreret = 1/M_S_IR_filtreret*ones(1,M_S_IR_filtreret);          % MA-filter, filterkoefficienter
+yMA_IR_S_filtreret = filter(hMA_IR_S_filtreret,1,c);      % filtrerer inputsignal for Simons del
+
+%for c = 1:N-2
+    %if (yMA_IR_S[c]>yMA_IR_S[c-1]) && (yMA_IR_S[c]<yMA_IR_S[c+1])
+        
+    %end
+%end
+
+
+%% plotning af signal efter pålagt filter
+figure
+
+subplot(2,3,1:2)
+plot(time_S,RED_S), grid, hold on
+plot(time_S,yMA_R_S), grid, hold on
+plot(time_S,IR_S), grid, hold on
+plot(time_S,yMA_IR_S), grid, hold on
+plot(time_S,c, 'r'), grid, hold on
+plot(time_S,yMA_IR_S_filtreret, 'g');
+lgd = legend('original rød pære respons', 'filtreret rød pære respons', 'original IR respons', '3 koefficienters IR filter', 'højeste værdi af hvert 32 sample hertil', '280 koefficienters filter på max værdi af hvert 32 sample');
+title(lgd,'akser');
+xlim([-5 5]);
+title('Simons filter respons');
+xlabel('sekunder'), ylabel('volt');
